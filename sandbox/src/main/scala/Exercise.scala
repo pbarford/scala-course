f2e927cec98e9c34048ce27c080e9083d5b1a7e8
Paul Barford <Paul.Barford@paddypower.com>
2014-05-06 16:25:29 +0100

    currying examples

--- sandbox/src/main/scala/Exercise.scala
+++ sandbox/src/main/scala/Exercise.scala
@@ -14,11 +14,19 @@ object Exercise {
     println(product(x => x * x)(3,4))
     println(factorial(5))
 
-    println(product2(x => x * x)(3,4))
+    println("1: " + product2(x => { println("1.1: "+ x + " * " + x) ; x * x})(3,4))
     println(factorial2(5))
 
     println(mapReduce(x => { println("x="+x); x}, (x,y) => {println("(x):"+ x + "* (y):" + y); x * y}, 1)(3,6))
+
     println(mapReduce(x => { println("x="+x); x}, (x,y) => {println("(x):"+ x + "+ (y):" + y); x + y}, 0)(3,6))
+
+    println(mapReduce2((x,y) => {println("(x):"+ x + "* (y):" + y); x * y}, 1)(3,6))
+    println(mapReduce2((x,y) => {println("(x):"+ x + "* (y):" + y); x + y}, 0)(3,4))
+    println(mapReduce2((x,y) => {println("(x):"+ x + "* (y):" + y); x * y}, 1)(3,4))
+
+    println("2: " + product2(x => { println("2.1: "+ x + " + " + x) ; x + x})(3,4))
+    println(product3(3,4))
 
   }
 
@@ -46,11 +54,17 @@ object Exercise {
 
   def factorial(n: Int) = product(x => x)(1, n)
 
+  def mapReduce2(combine: (Int, Int) => Int, baseUnit: Int)(a: Int, b:Int):Int =
+    if(a>b) baseUnit
+    else combine(a, mapReduce2(combine, baseUnit)(a+1, b))
+
   def mapReduce(f: Int => Int, combine: (Int, Int) => Int, baseUnit: Int)(a: Int, b:Int):Int =
   if(a>b) baseUnit
   else combine(f(a), mapReduce(f, combine, baseUnit)(a+1, b))
 
-  def product2(f: Int => Int)(a:Int, b: Int): Int = mapReduce(f, (x,y) => x + y, 1)(a,b)
+  def product2(f: Int => Int)(a:Int, b: Int): Int = mapReduce(f, (x,y) => x * y, 1)(a,b)
 
   def factorial2(n: Int) = product2(x => x)(1, n)
+
+  def product3(a:Int, b: Int): Int = mapReduce2((x,y) => x + y, 0)(a,b)
 }
